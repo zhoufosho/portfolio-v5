860900d3d498a19ebe41393badf72444446b71f8
Elaine Zhou <elainee.zhou@gmail.com>
2021-05-05 11:13:29 -0700

    Updates

---
+++ precache-manifest.8b78031268562275d19e148ee7bd68b6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17408c80878f6a4a2049a0367c061db",
+    "url": "/portfolio-v5/index.html"
+  },
+  {
+    "revision": "412092868b9091e49d53",
+    "url": "/portfolio-v5/static/css/main.613b65dc.chunk.css"
+  },
+  {
+    "revision": "01073a1da26e93440f3e",
+    "url": "/portfolio-v5/static/js/2.eb90beb6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio-v5/static/js/2.eb90beb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "412092868b9091e49d53",
+    "url": "/portfolio-v5/static/js/main.1b169d19.chunk.js"
+  },
+  {
+    "revision": "18cbc3683a6172388169",
+    "url": "/portfolio-v5/static/js/runtime-main.ae72296a.js"
+  },
+  {
+    "revision": "c07c916c55ef23e1f0a0dbcb10b9aaae",
+    "url": "/portfolio-v5/static/media/Karla-Bold.c07c916c.ttf"
+  },
+  {
+    "revision": "740dfd331d76c6cd37793423cb1e0cad",
+    "url": "/portfolio-v5/static/media/Karla-Italic.740dfd33.ttf"
+  },
+  {
+    "revision": "1b55fee684d61bfeaa762684931b1bc9",
+    "url": "/portfolio-v5/static/media/Karla-Regular.1b55fee6.ttf"
+  },
+  {
+    "revision": "0c6b8e07701bde6d661902803104aab1",
+    "url": "/portfolio-v5/static/media/PTMono-Regular.0c6b8e07.ttf"
+  }
+]);
